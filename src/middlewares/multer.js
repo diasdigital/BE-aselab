40959a870768b537edd564753068e65833c194d1
Dias <78405123+diasdigital@users.noreply.github.com>
2024-09-18 09:40:50 +0700

    filter hanya menerima image

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -11,18 +11,26 @@ const storage = multer.diskStorage({
     },
 });
 
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'image/jpeg' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/png'
+    ) {
+        cb(null, true);
+    } else {
+        const errMsg = 'Hanya boleh upload gambar (jpeg, jpg, png)';
+        req.fileValidationError = errMsg;
+        return cb(new Error(errMsg), false);
+    }
+};
+
 const upload = multer({
     storage: storage,
+    fileFilter: fileFilter,
     limits: {
         fileSize: 3 * 1000 * 1000,
     },
 });
-
-// const upload = multer({
-//     storage: multer.memoryStorage(),
-//     limits: {
-//         fileSize: 3 * 1000 * 1000,
-//     },
-// });
 
 module.exports = upload;
