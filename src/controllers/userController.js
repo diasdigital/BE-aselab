ccf7817f4ea76339a2f735eec00f56d6b9ba78e5
Dias <78405123+diasdigital@users.noreply.github.com>
2024-09-19 13:32:47 +0700

    hash password ketika edit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/user.js');
+const bcrypt = require('bcrypt');
 const upload = require('../middlewares/multer.js').single('img');
 
 const getUserLoggedIn = async (req, res) => {
@@ -14,8 +15,6 @@ const getUserLoggedIn = async (req, res) => {
         res.status(500).json({ message: error.message, data: null });
     }
 };
-
-// const getAllUsers = async (req, res) => {
 //     try {
 //         const response = await User.findAll({
 //             attributes: ['email', 'username', 'phone_number', 'office_address'],
@@ -43,19 +42,27 @@ const getUserLoggedIn = async (req, res) => {
 const updateUser = async (req, res) => {
     try {
         upload(req, res, async (err) => {
-            if (req.fileValidationError) {
-                return res
-                    .status(400)
-                    .json({ message: req.fileValidationError });
-            }
-            if (err?.code === 'LIMIT_FILE_SIZE') {
-                return res
-                    .status(413)
-                    .json({ message: 'File terlalu besar. Max 3MB' });
+            if (req.file) {
+                if (req.fileValidationError) {
+                    return res
+                        .status(400)
+                        .json({ message: req.fileValidationError });
+                }
+                if (err?.code === 'LIMIT_FILE_SIZE') {
+                    return res
+                        .status(413)
+                        .json({ message: 'File terlalu besar. Max 3MB' });
+                }
+
+                // masukin filename kalo ada file yang diupload
+                req.body.img = req.file.filename;
             }
 
-            // masukin filename kalo ada file yang diupload
-            req.body.img = req.file?.filename;
+            if (req.body.password) {
+                const hashedPwd = await bcrypt.hash(req.body.password, 10);
+                req.body.password = hashedPwd;
+            }
+
             await User.update(req.body, {
                 where: {
                     user_id: req.user_id,
@@ -70,7 +77,5 @@ const updateUser = async (req, res) => {
 
 module.exports = {
     getUserLoggedIn,
-    // getAllUsers,
-    // getUserById,
     updateUser,
 };
