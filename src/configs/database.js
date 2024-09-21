710db72652386c52ce8d47cc73d1e97fe39adcee
Dias <78405123+diasdigital@users.noreply.github.com>
2024-09-21 13:23:21 +0700

    menambah setting url untuk DB

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,14 +1,6 @@
 const mysql = require('mysql2');
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-    }
-);
+const db = new Sequelize(process.env.DB_URL);
 
 module.exports = db;
