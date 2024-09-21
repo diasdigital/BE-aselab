537f16c6efb92d96b75c6ade450b63b2682ad402
Dias <78405123+diasdigital@users.noreply.github.com>
2024-09-21 13:33:19 +0700

    menambah dialect mysql

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.DB_URL);
+const db = new Sequelize(process.env.DB_URL, { dialect: 'mysql' });
 
 module.exports = db;
